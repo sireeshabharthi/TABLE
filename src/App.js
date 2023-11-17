9d51245d63e24fad002aa2c2eaeb4970174642b0
sireeshabharthi <125027819+sireeshabharthi@users.noreply.github.com>
2023-11-17 20:51:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import SearchTable from './components/filterTable';
+
+function App() {
+  return (
+    <div className="App">
+     <SearchTable></SearchTable> 
+       
+    </div>
+  );
+}
+
+export default App;
